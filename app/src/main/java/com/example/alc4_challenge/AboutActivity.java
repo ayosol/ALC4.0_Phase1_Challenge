6d384e4d5c3b14b3cd6cc12df525848a3d159d90
Solomon A. Ogunbowale <solomonayodeleo@gmail.com>
2019-07-14 02:07:13 +0100

    Corrected the error and my webView is now displaying

--- app/src/main/java/com/example/alc4_challenge/AboutActivity.java
+++ app/src/main/java/com/example/alc4_challenge/AboutActivity.java
@@ -1,12 +1,15 @@
 package com.example.alc4_challenge;
 
 import android.content.Intent;
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -36,9 +39,12 @@ public class AboutActivity extends AppCompatActivity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.getAllowContentAccess();
-        webSettings.getCacheMode();
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+                handler.proceed();
+            }
+        });
 
 
     }
